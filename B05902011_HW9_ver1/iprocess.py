129ff9a65fb45cf11ef95d1bd57d829291366571
b05902011 <b05902011@linux10.csie.ntu.edu.tw>
2019-12-07 22:29:47 +0800

    prew test

--- B05902011_HW9_ver1/iprocess.py
+++ B05902011_HW9_ver1/iprocess.py
@@ -6,8 +6,8 @@ import math
 def expand(im_raw, size):
     X,Y = np.shape(im_raw)
     im = im_raw.copy().astype("int")
-    row = np.zeros((size, Y), dtype("int"))
-    col = np.zeros((X+2, size), dtype("int"))
+    row = np.zeros((size, Y), dtype = "int")
+    col = np.zeros((X+2, size), dtype = "int")
     im = np.concatenate((row, im), axis = 0)
     im = np.concatenate((im, row), axis = 0)
     im = np.concatenate((col, im), axis = 1)
@@ -19,7 +19,7 @@ def Robo(im_raw, threshold):
     img = expand(im_raw, 1)
     f1 = lambda x : (img[x[0] + 1, x[1] + 1] - img[x])**2
     f2 = lambda x : (img[x[0] + 1, x[1] - 1] - img[x])**2 
-    f1 = lambda x : math.sqrt(f1(x) + f2((x[0], x[1]+1)))
+    f = lambda x : math.sqrt(f1(x) + f2((x[0], x[1]+1)))
     for i in range(X):
         for j in range(Y):
             img[i+1, j+1] = 255 * (f((i+1, j+1)) < threshold )
@@ -27,10 +27,16 @@ def Robo(im_raw, threshold):
 
 def Prew(im_raw, threshold):
     X,Y = np.shape(im_raw)
-    img = expand(im_raw, 2)
-    f1 = lambda x : (sum(img[x[0] + 2][x[1] : x[1] + 3]) 
-                    - sum(img[x[0]][x[1] : x[1] + 3]))**2
-    f2 = lambda x : 
+    img = expand(im_raw, 1)
+    f1 = lambda x : (sum(img[x[0] + 1, x[1] - 1 : x[1] + 2]) 
+                    - sum(img[x[0] - 1, x[1] - 1 : x[1] + 2]))**2
+    f2 = lambda x : (sum(img[x[0] - 1 : x[0] + 2, x[1] + 1])
+                    - sum(img[x[0] - 1 : x[0] + 2, x[1] - 1]))**2
+    f = lambda x : math.sqrt(f1(x) + f2(x))
+    for i in range(X):
+        for j in range(Y):
+            img[i + 1, j + 1] = 255 * (f((i + 1, j + 1)) < threshold)
+    return img[1:X+1, 1:Y+1]
 
 def main(argv):
     img = np.array(Image.open(argv[1]))
